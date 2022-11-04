7974c3051553aa6212c72982b293395eafa99622
vonSch4 <115642881+vonSch4@users.noreply.github.com>
2022-11-04 12:20:32 +0500

    add swiper
    добавлен свайпер

---
+++ swiper/src/js/script-swiper.js
@@ -0,0 +1,97 @@
+(function() {
+
+    'use strict';
+
+    // точка останова, где swiper будет уничтожен
+    const breakpoint = window.matchMedia( '(min-width: 768px)' );
+
+    // следите за экземплярами swiper, чтобы уничтожить их позже
+    let mySwiper;
+
+
+    const breakpointChecker = function() {
+
+      // если экран > 768px
+      if ( breakpoint.matches === true ) {
+
+        // очистить старые экземпляры и встроенные стили, когда они доступны
+        if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+
+        // или ничего не делать
+        return;
+
+        // в противном случае при экране < 768px
+        } else if ( breakpoint.matches === false ) {
+
+          // включить swiper
+          return enableSwiper();
+
+        }
+  
+    };
+
+
+    const enableSwiper = function() {
+
+      mySwiper = new Swiper ('.swiper', {
+
+        spaceBetween: 16,
+        slidesPerView: 1.15,
+
+        grabCursor: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            370: {
+                slidesPerView: 1.5,
+            },
+
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            680: {
+                slidesPerView: 2.5,
+            }
+        }
+
+      });
+
+    };
+
+
+    // следить за изменениями размера экрана
+    breakpoint.addListener(breakpointChecker);
+
+    // начало
+    breakpointChecker();
+
+})();
+
+// кнопки показать/скрыть
+let showButton = document.querySelector('.brand-menu__show-all');
+let hideButton = document.querySelector('.brand-menu__hide');
+
+// меню брендов
+let showAllBrand = document.querySelector('.swiper__container');
+
+
+// открытие меню брендов
+showButton.addEventListener('click', function () {
+    showAllBrand.classList.add('swiper__container--open');
+    showButton.classList.add('brand-menu__show-all--hidden');
+    hideButton.classList.remove('brand-menu__hide--hidden');
+});
+
+
+// закрытие меню брендов
+hideButton.addEventListener('click', function () {
+    showAllBrand.classList.remove('swiper__container--open');
+    hideButton.classList.add('brand-menu__hide--hidden');
+    showButton.classList.remove('brand-menu__show-all--hidden');
+})
